c11799117220cdfa38c22bcc8b05c02e9bf21bc9
kaiky2303 <vieirakaiky23@gmail.com>
2026-04-10 08:40:50 -0300

    commit
    commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,15 +5,17 @@ export default function HomePage() {
   return (
     <div>
       <nav>
-        <a href="/" className="Mains">
+        <div className="Mains">
           <img src={Logo} width={50} height={50} alt="Logo" />
           <span>SiloTech</span>
-        </a>
+        </div>
+
         <ul className="url">
           <li><a href="/">Dashboard</a></li>
           <li><a href="/">Sobre nós</a></li>
           <li><a href="/">Contato</a></li>
         </ul>
+
         <div className="Entrada">
           <a href="/" className="asd">Fazer Login</a>
           <a href="/" className="asd">Iniciar Agora</a>
@@ -25,7 +27,7 @@ export default function HomePage() {
           <h1>Diminuindo suas perdas de grãos.</h1>
           <p>Reduza perdas, aumente a qualidade e tenha controle total do armazenamento com tecnologia.</p>
         </div>
-          <img src={Hero} alt="Imagem de Silo de Grãos"/>
+        <img src={Hero} alt="Imagem de Silo de Grãos"/>
       </section>
     </div>
   );
