fc18ec9c1f6a8de686738fc8b2e2c84310f69732
kaiky2303 <vieirakaiky23@gmail.com>
2026-04-22 15:16:36 -0300

    contato
    contatin

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Home from "./Home.jsx"
 import Navbar from "./Navbar.jsx";
+import Contato from "./contato.jsx"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/contato" element={<Contato />} />
       </Routes>
     </BrowserRouter>
   );
